9ab0cd4aa55e61316aa44dfdaac563911f3481b4
Reece <Reece@10.20.97.165>
2016-10-05 17:58:24 +0100

    added map controller and stuff

---
+++ gamedev-utils/src/gameMapController/Map.java
@@ -0,0 +1,7 @@
+package gameMapController;
+
+public class Map {
+
+	
+	
+}
---
+++ gamedev-utils/src/gameMapController/MapController.java
@@ -0,0 +1,12 @@
+package gameMapController;
+
+import java.util.ArrayList;
+
+import entities.Entity;
+
+public class MapController {
+
+	private ArrayList<Map> levels;
+	private ArrayList<Entity> enemies;
+	
+}
