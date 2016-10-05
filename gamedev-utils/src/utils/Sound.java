5394ab27777e0d613092fd8e80bae052a31470dd
mxw596 <mxw596@cca-ug04-13027.cs.bham.ac.uk>
2016-10-05 12:58:04 +0100

    sound

---
+++ gamedev-utils/src/utils/Sound.java
@@ -0,0 +1,64 @@
+package utils;
+
+import java.io.IOException;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+
+public class Sound {
+	
+	public static final Sound laser = new Sound("sound/laser/laser.wav");
+	public static final Sound explosion = new Sound("sound/explosion/explosion.wav");
+	public static final Sound ghostHurt = new Sound("sound/ghost_hurt.wav");
+	public static final Sound ghost = new Sound("sound/ghost.wav");
+	public static final Sound ghostLaughter = new Sound("sound/ghost_laughter.wav");
+	
+	private Clip clip, clip1, clip2;
+	
+	public Sound(String path){
+		try {
+			AudioInputStream sound = ResourceLoader.getSound(path);
+			DataLine.Info info = new DataLine.Info(Clip.class, sound.getFormat());
+			clip = (Clip) AudioSystem.getLine(info);
+			clip.open(sound);
+			clip1 = (Clip) AudioSystem.getLine(info);
+			clip1.open(sound);
+			clip2 = (Clip) AudioSystem.getLine(info);
+			clip2.open(sound);
+		} catch (LineUnavailableException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void play(){
+		if(clip.isActive()){
+			if(clip1.isActive()){
+				if(clip2.isActive()){
+					clip.setFramePosition(0);
+					clip.start();
+				}else{
+					clip2.setFramePosition(0);
+					clip2.start();
+				}
+			}else{
+				clip1.setFramePosition(0);
+				clip1.start();
+			}
+		}else{
+			clip.setFramePosition(0);
+			clip.start();
+		}
+	}
+	
+	public void stop(){
+		clip.stop();
+	}
+	
+	public void loop(){
+		clip.loop(Clip.LOOP_CONTINUOUSLY);
+	}
+
+}
