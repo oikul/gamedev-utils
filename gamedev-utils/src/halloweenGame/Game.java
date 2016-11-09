70f27f95b97f5ffa3f6a9c1176913ac2c588da81
Reece <Reece@AMD-A8-6600.lan>
2016-11-09 23:20:18 +0000

    added some updates to my physics thing

--- gamedev-utils/src/halloweenGame/Game.java
+++ gamedev-utils/src/halloweenGame/Game.java
@@ -7,6 +7,7 @@ import java.awt.event.KeyEvent;
 import java.awt.geom.Point2D;
 import java.util.Random;
 
+import org.lwjgl.util.vector.Matrix2f;
 import org.lwjgl.util.vector.Vector2f;
 
 import handlers.MathHandler;
@@ -22,7 +23,7 @@ public class Game {
 	private Vector2f[] movingAway;
 
 	public Game() {
-		int numofenemies = 10;
+		int numofenemies = 1000;
 		enemies = new Vector2f[numofenemies];
 		enemiesMomentum = new Vector2f[numofenemies];
 		movingTo = new Vector2f[numofenemies];
@@ -30,16 +31,26 @@ public class Game {
 		playerLoc = new Vector2f(Main.width / 2, Main.height / 2);
 		Random rad = new Random();
 
-		for (int x = 0; x < enemies.length; x++) {
-			enemies[x] = new Vector2f(rad.nextInt(Main.width) + 0.0000000001f,
-					rad.nextInt(Main.height) + 0.0000000001f);
-			movingTo[x] = new Vector2f();
-			movingAway[x] = new Vector2f();
-			enemiesMomentum[x] = new Vector2f();
-		}
-
 		playerSpeed = 100;
 		enemySpeed = 90;
+		int size = 500;
+		for (int x = 0; x < enemies.length; x++) {
+			enemies[x] = new Vector2f(rad.nextInt(size)+Main.width/2-size/2,
+					rad.nextInt(size)+Main.height/2-size/2);
+			movingTo[x] = new Vector2f();
+			movingAway[x] = new Vector2f();
+			enemiesMomentum[x] = new Vector2f(Main.width/2-enemies[x].x+0.0000001f, Main.height/2-enemies[x].y+0.0000001f);
+			float length = enemiesMomentum[x].length();
+			enemiesMomentum[x].normalise();
+			enemiesMomentum[x].scale(length/20);
+			Matrix2f rotate = new Matrix2f();
+			rotate.setZero();
+			rotate.m01 = -1;
+			rotate.m10 = 1;
+			Matrix2f.transform(rotate, enemiesMomentum[x], enemiesMomentum[x]);
+			
+		}
+
 	}
 
 	public void update(float time, Vector2f mouseLocation) {
@@ -87,7 +98,7 @@ public class Game {
 			// the vector away from the green square
 //			Vector2f.sub(enemies[x], playerLoc, movingAway[x]);
 			// the vector towards the mouse
-			Vector2f.sub(mouseLocation, enemies[x], movingTo[x]);
+			Vector2f.sub(playerLoc, enemies[x], movingTo[x]);
 
 			float forceTo, forceAway = 0;
 			forceTo = movingTo[x].length();
@@ -108,22 +119,23 @@ public class Game {
 			if (movingTo[x].length() != 0) {
 
 				movingTo[x].normalise();
-				movingTo[x].scale(magnitude);
+				movingTo[x].scale(magnitude/10);
 
 			}
 			enemiesMomentum[x].translate(movingTo[x].x, movingTo[x].y);
 
 			enemies[x].translate(enemiesMomentum[x].x, enemiesMomentum[x].y);
+			
 			if (enemies[x].x < 0) {
 				enemies[x].x = 0;
 			} else if (enemies[x].x > Main.width) {
 				enemies[x].x = Main.width;
 			}
-			if (enemies[x].y < 0) {
-				enemies[x].y = 0;
-			} else if (enemies[x].y > Main.height) {
-				enemies[x].y = Main.height;
-			}
+//			if (enemies[x].y < 0) {
+//				enemies[x].y = 0;
+//			} else if (enemies[x].y > Main.height) {
+//				enemies[x].y = Main.height;
+//			}
 		}
 
 	}
@@ -131,12 +143,13 @@ public class Game {
 	public void draw(Graphics g) {
 
 		for (int x = 0; x < enemies.length; x++) {
-			g.setColor(Color.red);
-			g.fillRect((int) enemies[x].x - 8, (int) enemies[x].y - 8, 16, 16);
+			g.setColor(Color.blue);
+			if(x % 2 == 0)g.setColor(Color.darkGray);
+			g.fillRect((int) enemies[x].x - 2, (int) enemies[x].y - 2, 4, 4);
 
 		}
 		g.setColor(Color.green);
-		g.fillRect((int) playerLoc.x - 16, (int) playerLoc.y - 16, 32, 32);
+//		g.fillRect((int) playerLoc.x - 16, (int) playerLoc.y - 16, 32, 32);
 
 	}
 
