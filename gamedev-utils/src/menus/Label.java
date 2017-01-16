b1a3362495dc56d9ff7eae29d063c919e2ceb3bc
mxw596 <mxw596@cca-ug04-12103.cs.bham.ac.uk>
2017-01-16 12:08:57 +0000

    worked on label

--- gamedev-utils/src/menus/Label.java
+++ gamedev-utils/src/menus/Label.java
@@ -10,8 +10,8 @@ public class Label extends JLabel {
 	private static final long serialVersionUID = 1L;
 	
 	public Label(String text, String fontName, int fontType, int fontSize, Color fontColor, int x, int y, int width, int height){
-		this.setText(text);
 		this.setFont(new Font(fontName, fontType, fontSize));
+		this.setText(text);
 		this.setForeground(fontColor);
 		this.setBounds(x, y, width, height);
 		this.setHorizontalAlignment(JLabel.CENTER);
