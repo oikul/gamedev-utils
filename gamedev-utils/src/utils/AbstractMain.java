4630790bb8b33bf107a4681414149a343207ea7f
mxw596 <mxw596@cca-ug04-12103.cs.bham.ac.uk>
2017-01-16 11:18:43 +0000

    limited fps to screen refresh rate

--- gamedev-utils/src/utils/AbstractMain.java
+++ gamedev-utils/src/utils/AbstractMain.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.awt.GraphicsEnvironment;
+
 import javax.swing.JFrame;
 
 public abstract class AbstractMain extends JFrame {
@@ -13,6 +15,9 @@ public abstract class AbstractMain extends JFrame {
 		long beforeTime, afterTime, deltaTime = 0;
 		long counter = System.nanoTime() + 1000000000;
 		int fps = 0;
+		final int maxFPS = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice()
+				.getDisplayModes()[0].getRefreshRate();
+		long minFrameTime = 1000000000l / maxFPS;
 		deltaTime = 1;
 		update(0);
 		while (running) {
@@ -22,6 +27,13 @@ public abstract class AbstractMain extends JFrame {
 			afterTime = System.nanoTime();
 			deltaTime = afterTime - beforeTime;
 			fps++;
+			if (deltaTime < minFrameTime) {
+				try {
+					Thread.sleep((minFrameTime - deltaTime) / 1000000l);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
 			if (System.nanoTime() > counter) {
 				counter += 1000000000;
 				System.out.println("fps: " + fps);
