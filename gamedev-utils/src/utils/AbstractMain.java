b287c48a2862c9b889579dc138b84f6417f5c257
mxw596 <mxw596@cca-ug04-13057.cs.bham.ac.uk>
2017-01-23 17:51:30 +0000

    improved fps

--- gamedev-utils/src/utils/AbstractMain.java
+++ gamedev-utils/src/utils/AbstractMain.java
@@ -16,7 +16,7 @@ public abstract class AbstractMain extends JFrame {
 		long counter = System.nanoTime() + 1000000000;
 		int fps = 0;
 		final int maxFPS = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice()
-				.getDisplayModes()[0].getRefreshRate();
+				.getDisplayModes()[0].getRefreshRate() + 9;
 		long minFrameTime = 1000000000l / maxFPS;
 		deltaTime = 1;
 		update(0);
